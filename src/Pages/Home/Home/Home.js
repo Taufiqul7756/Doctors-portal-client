19acb0f7f4d36278dc3d2c53b65f5b98b2da5b99
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-01 13:30:12 +0600

    Navigation and Services components imported

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Navigation from "../../Shared/Navigation/Navigation";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Navigation></Navigation>
+      <Services></Services>
     </div>
   );
 };
