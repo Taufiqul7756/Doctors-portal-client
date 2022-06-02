30e975adc6eeb9b653433a3082d41ec2664c1038
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-02 16:40:18 +0600

    Appointment components added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AppointmentBanner from "../../AppoimtmentBanner/AppointmentBanner";
 import Navigation from "../../Shared/Navigation/Navigation";
 import Services from "../Services/Services";
 
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Navigation></Navigation>
       <Services></Services>
+      <AppointmentBanner></AppointmentBanner>
     </div>
   );
 };
