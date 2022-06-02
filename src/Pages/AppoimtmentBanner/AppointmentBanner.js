ffb2c019283dbbee0e49262bf9553d2a3df19a44
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-03 00:22:57 +0600

    80% Appointment section Done

--- src/Pages/AppoimtmentBanner/AppointmentBanner.js
+++ src/Pages/AppoimtmentBanner/AppointmentBanner.js
@@ -4,20 +4,74 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import doctor from "../../../src/images/doctor.png";
 import bg from "../../../src/images/appointment-bg.png";
+import { Button } from "@mui/material";
 
-const appointmentBg = {
+const appointmentBanner = {
   background: `url(${bg})`,
+  marginTop: "150px",
+  backgroundColor: "rgba(100, 103, 118)",
+  backgroundBlendMode: "darken ,luminosity",
 };
 
 const AppointmentBanner = () => {
   return (
-    <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+    <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
-          <img style={{ width: "400px" }} src={doctor} alt="" />
+          <img
+            style={{ width: "400px", marginTop: "-110px" }}
+            src={doctor}
+            alt=""
+          />
         </Grid>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h6">Appointment</Typography>
+        <Grid
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            textAlign: "left",
+            alignItems: "center",
+          }}
+          item
+          xs={12}
+          md={6}
+        >
+          <Box>
+            <Typography
+              sx={{ mb: 5 }}
+              style={{ color: "rgba(78, 220, 232)" }}
+              variant="h6"
+            >
+              Appointment
+            </Typography>
+            <Typography
+              // sx={{ mb: 3 }}
+              style={{ color: "rgba(255, 255, 255)" }}
+              variant="h6"
+            >
+              Make an Appointment Today
+            </Typography>
+            <Typography
+              style={{
+                color: "rgba(255, 255, 255)",
+                fontSize: 14,
+                fontWeight: "300",
+              }}
+              variant="h6 "
+              sx={{ my: 5 }}
+            >
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut eum
+              voluptatum voluptatem est animi. Est veniam cumque nemo neque
+              voluptatibus.
+            </Typography>
+
+            <Button
+              sx={{ display: "flex" }}
+              style={{ marginTop: "25px" }}
+              variant="contained"
+            >
+              Learn More
+            </Button>
+          </Box>
         </Grid>
       </Grid>
     </Box>
